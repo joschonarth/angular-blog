dbbe763783c58d3def62f24d338b1c55cb554e07
joschonarth <joschonarth@gmail.com>
2024-08-24 23:00:40 -0300

    Update Angular Blog

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Saiu o novo filme",
+    "description": "Olá",
+    "photoCover": ""
+  },
+  {
+    "id": "2",
+    "title": "Saiu o novo filme",
+    "description": "Olá",
+    "photoCover": ""
+  }
+]
